7defe5323da83d2336400b1548addc9db44a213e
EkaterinaZenina <90308326+EkaterinaZenina@users.noreply.github.com>
2021-10-06 19:06:24 +0300

    Update and rename Main to Main.java

--- Main.java
+++ Main.java
@@ -2,10 +2,9 @@ public class Main {
     public static void main(String[] args) {
         int account = 100;
         boolean replenisment = true;
-        int replenishment = 700;
+        int replenishment = 1100;
         int bonus = 0;
-        if (replenishment >= 1000) {
-            bonus = 0;
+        if (replenishment > 1000) {
             bonus = replenishment / 100;
         }
         System.out.println( replenishment + bonus + account);
